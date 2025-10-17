7f724d60104eea38467a8ff8c7d19ed4b7ad67e4
emonpappu17 <emon.pappu.5@gmail.com>
2025-10-17 21:16:47 +0600

    Add: Pick Function for Query Parameters

--- src/app/helper/pick.ts
+++ src/app/helper/pick.ts
@@ -1,4 +1,20 @@
+// const pick = <T extends Record<string, unknown>, k extends keyof T>(obj: T, keys: k[]): Partial<T> => {
+//     const finalObject: Partial<T> = {};
+
+//     for (const key of keys) {
+//         if (obj && Object.hasOwnProperty.call(obj, key)) {
+//             finalObject[key] = obj[key]
+//         }
+//     }
+
+//     return finalObject;
+// }
+
+// export default pick;
+
 const pick = <T extends Record<string, unknown>, k extends keyof T>(obj: T, keys: k[]): Partial<T> => {
+    console.log({ obj, keys });
+
     const finalObject: Partial<T> = {};
 
     for (const key of keys) {
@@ -6,6 +22,8 @@ const pick = <T extends Record<string, unknown>, k extends keyof T>(obj: T, keys
             finalObject[key] = obj[key]
         }
     }
+
+    console.log({ finalObject });
 
     return finalObject;
 }
---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express"
+import { jwtHelper } from "../helper/jwtHelper";
+
+const auth = (...roles: string[]) => {
+    return async (req: Request & { user?: any }, res: Response, next: NextFunction) => {
+        try {
+            // console.log(req?.headers.authorization);
+            const token = req?.headers.authorization;
+
+            // console.log({ token });
+
+            if (!token) {
+                throw new Error("You are not authorized!")
+            }
+
+            const verifyUser = jwtHelper.verifyToken(token, "abc");
+
+            req.user = verifyUser;
+
+            if (roles.length && !roles.includes(verifyUser.role)) {
+                throw new Error("You are not authorized!")
+            }
+
+            next();
+        }
+        catch (err) {
+            next(err)
+        }
+    }
+}
+
+export default auth;
