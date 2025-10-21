47f9d5d09f009962dcee8b073ca7ffe789ddc674
emonpappu17 <emon.pappu.5@gmail.com>
2025-10-21 19:17:32 +0600

    add:Creating Schedule – Part 1

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { userRoutes } from '../module/user/user.routes';
 import { authRoutes } from '../module/auth/auth.routes';
+import { scheduleRoutes } from '../module/schedule/schedule.routes';
 
 
 const router = express.Router();
@@ -13,6 +14,10 @@ const moduleRoutes = [
     {
         path: '/auth',
         route: authRoutes
+    },
+    {
+        path: '/schedule',
+        route: scheduleRoutes
     }
 ];
 
