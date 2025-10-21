a45276b2e0a22c2d4e00a593916e8974f5045e97
emonpappu17 <emon.pappu.5@gmail.com>
2025-10-21 23:17:01 +0600

    add:Creating Schedule – Part 2

--- src/app/module/schedule/schedule.service.ts
+++ src/app/module/schedule/schedule.service.ts
@@ -1,8 +1,11 @@
 import { addMinutes, addHours, format } from "date-fns";
+import { prisma } from "../../shared/prisma";
 
 const insertIntoDB = async (payload: any) => {
     const { startTime, endTime, startDate, endDate } = payload
     const intervalTime = 30;
+
+    const schedules = [];
 
     const currentDate = new Date(startDate)
     const lastDate = new Date(endDate)
@@ -20,12 +23,37 @@ const insertIntoDB = async (payload: any) => {
         const endDateTime = new Date(
             addMinutes(
                 addHours(
-                    `${format(lastDate, "yyyy-mm-dd")}`,
+                    `${format(currentDate, "yyyy-mm-dd")}`,
                     Number(endTime.split(":")[0]) // 11:00
                 ),
                 Number(endTime.split(":")[1])
             )
         )
+
+        while (startDateTime < endDateTime) {
+            const slotStartDateTime = startDateTime; // 10:00
+            const slotEndDateTime = addMinutes(startDateTime, intervalTime); // 10:30
+
+            const scheduleData = {
+                startDateTime: slotStartDateTime,
+                endDateTime: slotEndDateTime
+            }
+
+            const existingSchedule = await prisma.schedule.findFirst({
+                where: scheduleData
+                //  {
+                //     startDateTime: scheduleData.startDateTime,
+                //     endDateTime: scheduleData.endDateTime
+                // }
+            })
+
+            if (!existingSchedule) {
+                const result = await prisma.schedule.create({
+                    data: scheduleData
+                });
+                schedules.push(result)
+            }
+        }
     }
 
     return payload;
