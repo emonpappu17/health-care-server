998bbd8f3edaa844e4bc402efaafbcb6b47edb40
emonpappu17 <emon.pappu.5@gmail.com>
2025-10-27 13:08:52 +0600

    Prisma Error Handling – Part 2

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -4,7 +4,7 @@ import httpStatus from "http-status"
 
 const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
 
-    let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+    let statusCode: number = httpStatus.INTERNAL_SERVER_ERROR;
     let success = false;
     let message = err.message || "Something went wrong!";
     let error = err;
@@ -14,16 +14,38 @@ const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFun
     if (err instanceof Prisma.PrismaClientKnownRequestError) {
         if (err.code === "P2002") {
             message = "Duplicate key error",
-                error = err.meta
+                error = err.meta,
+                statusCode = httpStatus.CONFLICT
         }
         if (err.code === "P1000") {
             message = "Authentication failed against database server",
-                error = err.meta
+                error = err.meta,
+                statusCode = httpStatus.BAD_GATEWAY
         }
         if (err.code === "P2003") {
             message = "Foreign key constraint failed",
-                error = err.meta
+                error = err.meta,
+                statusCode = httpStatus.BAD_REQUEST
         }
+    }
+
+    else if (err instanceof Prisma.PrismaClientValidationError) {
+        message = "Validation error",
+            error = err.message,
+            statusCode = httpStatus.BAD_REQUEST
+    }
+
+    else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+        message = "Unknown prisma error occurred!",
+            error = err.message
+        statusCode = httpStatus.BAD_REQUEST
+
+    }
+
+    else if (err instanceof Prisma.PrismaClientInitializationError) {
+        message = "Prisma client failed to initialized!",
+            error = err.message,
+            statusCode = httpStatus.BAD_REQUEST
     }
 
     res.status(statusCode).json({
