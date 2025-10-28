1aad448859580beaa7a6755e54ba4872d5214281
emonpappu17 <emon.pappu.5@gmail.com>
2025-10-28 14:19:53 +0600

    add: Creating Appointment – Part 2

--- src/app/module/appointment/appointment.service.ts
+++ src/app/module/appointment/appointment.service.ts
@@ -1,7 +1,30 @@
+import { fa } from "zod/v4/locales";
+import { prisma } from "../../shared/prisma";
 import { IJWTPayload } from "../../types/common";
 
 const createAppointment = async (user: IJWTPayload, payload: { doctorId: string, scheduleId: string }) => {
-    console.log({ user, payload });
+    // console.log({ user, payload });
+
+    const patientData = await prisma.patient.findUniqueOrThrow({
+        where: {
+            email: user.email
+        }
+    })
+
+    const doctorData = await prisma.doctor.findUniqueOrThrow({
+        where: {
+            id: payload.doctorId,
+            isDeleted: false
+        }
+    })
+
+    const isBookedOrNot = await prisma.doctorSchedules.findFirstOrThrow({
+        where: {
+            doctorId: payload.doctorId,
+            scheduleId: payload.scheduleId,
+            isBooked: false
+        }
+    })
 };
 
 export const AppointmentService = {
