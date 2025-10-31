ccb9141e9cd40127113fea1cf5a9a3730e0e2ebb
emonpappu17 <emon.pappu.5@gmail.com>
2025-10-31 12:11:46 +0600

    add: Handling Webhook & Updating Payment Status – Part 2

--- src/app/module/payment/payment.service.ts
+++ src/app/module/payment/payment.service.ts
@@ -5,17 +5,18 @@ import { PaymentStatus } from "@prisma/client";
 const handleStripeWebhookEvent = async (event: Stripe.Event) => {
     switch (event.type) {
         case "checkout.session.completed": {
-            const session = event.data.object as Stripe.Checkout.Session;
+            const session = event.data.object as any;
+            // const session = event.data.object as Stripe.Checkout.Session;
 
-            const appointmentId = session.metadata?.appointmentId
-            const paymentId = session.metadata?.paymentId
+            const appointmentId = session.metadata?.appointmentId;
+            const paymentId = session.metadata?.paymentId;
 
             await prisma.appointment.update({
                 where: {
                     id: appointmentId
                 },
                 data: {
-                    paymentStatus: session.payment_status === 'paid' ? PaymentStatus.PAID : PaymentStatus.UNPAID
+                    paymentStatus: session.payment_status === "paid" ? PaymentStatus.PAID : PaymentStatus.UNPAID
                 }
             })
 
@@ -24,35 +25,20 @@ const handleStripeWebhookEvent = async (event: Stripe.Event) => {
                     id: paymentId
                 },
                 data: {
-                    status: session.payment_status === 'paid' ? PaymentStatus.PAID : PaymentStatus.UNPAID
+                    status: session.payment_status === "paid" ? PaymentStatus.PAID : PaymentStatus.UNPAID,
+                    paymentGatewayData: session
                 }
             })
 
-
-            console.log("✅ Payment completed, appointment confirmed.");
-            console.log({ session });
-
-            break;
-        }
-
-        case "checkout.session.expired":
-        case "payment_intent.payment_failed": {
-            const session = event.data.object as any;
-
-            // await prisma.payment.updateMany({
-            //     where: { stripeSession: session.id },
-            //     data: { status: "failed" },
-            // });
-
-            console.log("⚠️ Payment failed or expired.");
             break;
         }
 
         default:
-            console.log(`⚙️ Unhandled event type: ${event.type}`);
+            console.log(`ℹ️ Unhandled event type: ${event.type}`);
     }
-}
+};
 
 export const PaymentService = {
     handleStripeWebhookEvent
 }
+
