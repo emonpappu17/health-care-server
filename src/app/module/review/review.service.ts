32c3137e65ef56203ad9dc02eca15fef31edc4f5
emonpappu17 <emon.pappu.5@gmail.com>
2025-11-01 11:50:13 +0600

    add: Creating Review – Part 2

--- src/app/module/review/review.service.ts
+++ src/app/module/review/review.service.ts
@@ -1,8 +1,8 @@
+import httpStatus from "http-status";
 import ApiError from "../../errors/ApiError";
 import { IOptions } from "../../helper/paginationHelper";
 import { prisma } from "../../shared/prisma";
 import { IJWTPayload } from "../../types/common";
-import httpStatus from "http-status"
 
 const insertIntoDB = async (user: IJWTPayload, payload: any) => {
 
@@ -21,8 +21,39 @@ const insertIntoDB = async (user: IJWTPayload, payload: any) => {
 
     if (patientData.id !== appointmentData.patientId) {
         throw new ApiError(httpStatus.BAD_REQUEST, "This is not your appointment")
-    }
+    };
 
+    return await prisma.$transaction(async (tnx) => {
+        const result = await tnx.review.create({
+            data: {
+                appointmentId: appointmentData.id,
+                doctorId: appointmentData.doctorId,
+                patientId: appointmentData.patientId,
+                rating: payload.rating,
+                comment: payload.comment
+            }
+        });
+
+        const avgRating = await tnx.review.aggregate({
+            where: {
+                doctorId: appointmentData.doctorId
+            },
+            _avg: {
+                rating: true
+            }
+        });
+
+        await tnx.doctor.update({
+            where: {
+                id: appointmentData.doctorId
+            },
+            data: {
+                averageRating: avgRating._avg.rating as number
+            }
+        });
+
+        return result;
+    })
 };
 
 
