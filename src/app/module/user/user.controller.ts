1b8026b0657c3442d9dc6c200cbf32de2e1ebb91
emonpappu17 <emon.pappu.5@gmail.com>
2025-11-02 11:34:08 +0600

    add: updateMyProfile api

--- src/app/module/user/user.controller.ts
+++ src/app/module/user/user.controller.ts
@@ -84,6 +84,20 @@ const changeProfileStatus = catchAsync(async (req: Request, res: Response) => {
     })
 });
 
+const updateMyProfile = catchAsync(async (req: Request & { user?: IJWTPayload }, res: Response) => {
+
+    const user = req.user;
+
+    const result = await UserService.updateMyProfile(user as IJWTPayload, req);
+
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "My profile updated!",
+        data: result
+    })
+});
+
 
 export const UserController = {
     createPatient,
@@ -91,5 +105,6 @@ export const UserController = {
     createDoctor,
     getAllFromDB,
     getMyProfile,
-    changeProfileStatus
+    changeProfileStatus,
+    updateMyProfile
 }
